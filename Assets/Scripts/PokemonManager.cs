9906e46dd47fb703bf5998d801e864d86298480d
LAPTOP-N0FT603V\rouge <rouge2504@gmail.com>
2020-06-21 14:30:45 -0500

    First Commit

---
+++ Assets/Scripts/PokemonManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PokemonManager : MonoBehaviour
+{
+    public enum Tipo { FUEGO, AGUA, PLANTA, FANTASMA};
+
+    Pokemon charmander;
+    public TemplatePokemon[] misPokemon;
+    public SpriteRenderer backImageSprite;
+    private int iterador;
+
+    public StatusPokemon statusPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iterador = 0;
+        backImageSprite.sprite = misPokemon[iterador].backImage;
+
+        statusPlayer.nombre.text = misPokemon[iterador].name;
+
+        print("Largo del arreglo: " + misPokemon.Length);
+
+        for (int i = 0; i < misPokemon.Length; i++)
+        {
+            print("El nombre del pokemon es: " + misPokemon[i].name);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            iterador--;
+            backImageSprite.sprite = misPokemon[iterador].backImage;
+            statusPlayer.nombre.text = misPokemon[iterador].name;
+            print("Aprentando tecla");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            iterador++;
+            backImageSprite.sprite = misPokemon[iterador].backImage;
+            statusPlayer.nombre.text = misPokemon[iterador].name;
+            print("Aprentando tecla");
+        }
+
+    }
+
+
+}
+
---
+++ Assets/Scripts/TemplatePokemon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Pokemon", menuName = "Pokemons/Pokemon")]
+
+public class TemplatePokemon : ScriptableObject
+{
+    public string name;
+    public int nivel;
+    public int ataque;
+    public int defensa;
+    public string[] ataques;
+    public Sprite frontImage;
+    public Sprite backImage;
+
+    public PokemonManager.Tipo type;
+}
